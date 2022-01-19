e7e5aee8cdf60372fc51428607f279fcdf1b923f
Cenk Öztürk <ccenkozturrk@outlook.com>
2022-01-19 21:20:34 +0300

    DbContext file has been created. Necessary frameworks have been added. Added to services.

---
+++ JWTokenAuthDemo/IdentityAuth/ApplicationUser.cs
@@ -0,0 +1,14 @@
+﻿using JWTokenAuthDemo.DbContext;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JWTokenAuthDemo.IdentityAuth
+{
+    public class ApplicationUser : IdentityUser
+    {
+       
+    }
+}
