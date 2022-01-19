7d06f7ea79af5b65d052156228f285cd65034b3c
Cenk Öztürk <ccenkozturrk@outlook.com>
2022-01-19 21:51:53 +0300

    Classes created. Connection to database established. Controller created.

---
+++ JWTokenAuthDemo/Controllers/AuthenticateController.cs
@@ -0,0 +1,32 @@
+﻿using JWTokenAuthDemo.IdentityAuth;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JWTokenAuthDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticateController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly IConfiguration _configuration;
+
+        public AuthenticateController(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager, IConfiguration configuration)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _configuration = configuration;
+        }
+
+
+        
+
+    }
+}
---
+++ JWTokenAuthDemo/Models/LoginModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JWTokenAuthDemo.Models
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage ="Username is requied")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage ="Password is required")]
+        public string Password { get; set; }
+    }
+}
