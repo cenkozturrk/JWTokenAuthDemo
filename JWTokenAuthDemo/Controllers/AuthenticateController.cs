6a78f224d0a946df137707179ed3ea20bb285d57
Cenk Öztürk <ccenkozturrk@outlook.com>
2022-01-26 15:37:11 +0300

    Registerregister-admin method completed

--- JWTokenAuthDemo/Controllers/AuthenticateController.cs
+++ JWTokenAuthDemo/Controllers/AuthenticateController.cs
@@ -51,5 +51,39 @@ namespace JWTokenAuthDemo.Controllers
 
         }
 
+
+        [HttpPost]
+        [Route("Register-admin")]
+        public async Task<IActionResult> RegisterAdmin([FromBody] RegisterModel model)
+        {
+            var userExist = await _userManager.FindByNameAsync(model.UserName);
+            if (userExist != null)
+                return StatusCode(StatusCodes.Status500InternalServerError, new Response { Status = "Error", Message = "User already exists!" });
+
+            ApplicationUser user = new()
+            {
+                Email = model.Email,
+                SecurityStamp = Guid.NewGuid().ToString(),
+                UserName = model.UserName,                
+            };
+
+            var result = await _userManager.CreateAsync(user, model.Password);
+            if (!result.Succeeded)
+                return StatusCode(StatusCodes.Status500InternalServerError, new Response { Status = "Error", Message = "User creation failed! Please try again" });
+
+            if (!await _roleManager.RoleExistsAsync(UserRoles.Admin))
+                await _roleManager.CreateAsync(new IdentityRole(UserRoles.Admin));
+
+            if (!await _roleManager.RoleExistsAsync(UserRoles.User))
+                await _roleManager.CreateAsync(new IdentityRole(UserRoles.User));
+
+            if (await _roleManager.RoleExistsAsync(UserRoles.Admin))
+                await _userManager.AddToRoleAsync(user, UserRoles.Admin);
+
+            return Ok(new Response { Status = "Success", Message = "User created successfully" });
+
+        }
+
+
     }
 }
